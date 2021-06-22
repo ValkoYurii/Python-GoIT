607891b8bda48e1a16e2e43d977f15bca6bac723
ValkoYurii <80618892+ValkoYurii@users.noreply.github.com>
2021-06-22 15:39:31 +0300

    Add files via upload

---
+++ ProjectZeta/setup.py
@@ -0,0 +1,12 @@
+setup(
+    name = 'project_zeta',
+    version = '1',
+     entry_points={
+        'console_scripts': [
+                'project_zeta = project_zeta:main',
+            ]
+    },
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    description= 'project_zeta'
